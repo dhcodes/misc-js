f4e134d8fb935a682aab0199de364bf144df424d
dhcodes <thor.dh@gmail.com>
2017-01-02 07:39:59 -0600

    copyWithin array method

---
+++ copyWithin.js
@@ -0,0 +1,20 @@
+//Array.prototype.copyWithin()
+
+//Usage: arr.copyWithin(target, start, end)
+//Optional: start, end
+
+let arr = [0, 'meow', 1, 'ruff'];
+console.log(arr);
+/*
+console.log("\narr.copyWithin(0):")
+console.log(arr.copyWithin(0))
+/*
+console.log("\narr.copyWithin(2):")
+console.log(arr.copyWithin(2))
+*/
+/*
+console.log("\narr.copyWithin(1, 3, 4):")
+console.log(arr.copyWithin(1, 3, 4))
+*/
+console.log("\narr.copyWithin(-1, 0, 1):")
+console.log(arr.copyWithin(-1, 0, 1))
