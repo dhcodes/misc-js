98e9cc6cd0a75c66f2397ff230e5b1d181911c41
dhcodes <thor.dh@gmail.com>
2017-01-06 21:34:58 -0600

    array-pop.js

---
+++ array-pop.js
@@ -0,0 +1,16 @@
+console.log("Array.pop() will remove the last item in an array and return it. It also works on arguments and HTML nodes")
+
+console.log("let arr = [2, 3, 2, 8, 'moo', 'cluck']")
+let arr = [2, 3, 2, 8, 'moo', 'cluck']
+
+console.log("\narr.pop()");
+console.log(arr.pop())
+
+console.log("\narr now is:", arr)
+
+console.log("\nlet arr2 = []");
+let arr2 = [];
+
+console.log("arr2.pop()");
+console.log(arr2.pop())
+console.log("\narr2 still equals:", arr2)
